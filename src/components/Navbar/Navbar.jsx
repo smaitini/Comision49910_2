8e6e560ff73480f99c4447cc8ed6887b5a0ef244
smaitini <smaitini@gmail.com>
2023-11-30 21:54:45 -0300

    Tailwind

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,30 @@
-
-import CartWidget from "../CartWidget/CartWidget"
-import "./Navbar.css"
-import carrito from "/carrito.png"
-import logo from "/SM_logo.png" //"../../../public/SM_logo.png"
-
+import './Navbar.css'
+import CartWidget from "../CartWidget/CartWidget";
+import carrito from "/carrito.png";
+import logo from "/SM_logo.png"; //"../../../public/SM_logo.png"
+import NavLink from './NavLink'
 
 const Navbar = () => {
   return (
-    <header className="header_container">
-      <img className="header__img" src={logo} alt="logo" />
-      <nav>
-        <ul className="nav_container">
-          <li className="nav__list"><a href="#">Productos</a></li>
-          <li className="nav__list"><a href="#">Contactenos</a></li>
-          <li className="nav__list"><a href="#">Historia</a></li>
-          <li className="nav__list"><a href="carrito"><CartWidget imagen={carrito} alt="carrito"/></a></li>
-        </ul>
-      </nav>
+    <header className="bg-purpura">
+        <div className="container m-auto py-6 flex justify-between items-center">
+        <img src={logo} alt="logo" />
+          <nav className="flex gap-4">
+                    <NavLink href={"#"} text={"Enlace 1"}/>
+                    <NavLink href={"#"} text={"Enlace 2"}/>
+                    <NavLink href={"#"} text={"Enlace 3"}/>
+                    <NavLink href={"#"} text={"Enlace 4"}/>
+                    <NavLink href={"#"} text={"Enlace 5"}/>
+            <li className="nav__list">
+              <a href="carrito">
+                <CartWidget imagen={carrito} alt="carrito" />
+              </a>
+            </li>
+            </nav>
+        
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
